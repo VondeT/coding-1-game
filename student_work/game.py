546ce1dcce33ab1c1bda97777aaddd04f2ca9cdb
VondeT <s979083@student.knoxschools.org>
2026-02-27 15:14:23 +0000

    removed eagle from game data

--- student_work/game.py
+++ student_work/game.py
@@ -11,7 +11,6 @@ game_data = {
     'width': 5,
     'height': 5,
     'player': {"x": 0, "y": 0, "score": 0, "energy": 10, "max_energy": 10},
-    'eagle_pos': {"x": 4, "y": 4},
     'collectibles': [
         {"x": 2, "y": 1, "collected": False},
     ],
